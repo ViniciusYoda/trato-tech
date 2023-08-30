b6f0b49465bb13aa80d835c4ad6f5697467fceee
Vinícius Yoda <vyoda4604@gmail.com>
2023-08-30 16:02:15 -0300

    navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import NavBar from './components/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      Hello world
-    </div>
+    <NavBar />
   </React.StrictMode>
 );
