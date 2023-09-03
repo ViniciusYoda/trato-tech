a9f232a56ca138b60c4b9510e8c63b640fde9df4
Vinícius Yoda <vyoda4604@gmail.com>
2023-09-03 11:08:28 -0300

    actions

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -1,10 +1,16 @@
 import Header from 'components/Header';
+import Item from 'components/Item';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import styles from './Categoria.module.scss';
 
 export default function Categoria() {
   const { nomeCategoria } = useParams();
-  const categoria = useSelector(state => state.categorias.find(categoria => categoria.id === nomeCategoria));
+  const { categoria, itens } = useSelector(state => ({
+    categoria: state.categorias.find(categoria => categoria.id === nomeCategoria),
+    itens: state.itens.filter(item => item.categoria === nomeCategoria),
+  }));
+
   return (
     <div>
       <Header
@@ -12,6 +18,11 @@ export default function Categoria() {
         descricao={categoria.descricao}
         imagem={categoria.header}
       />
+      <div className={styles.itens}>
+        {itens?.map(item => (
+          <Item key={item.id} {...item} />
+        ))}
+      </div>
     </div>
   )
 }
