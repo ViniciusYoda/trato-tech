57f7aae88a7ee68ee728af2768d0c3515c711469
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-09-10 12:35:07 -0300

    db

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,35 @@ import Header from 'components/Header';
 import styles from './Home.module.scss';
 import relogio from 'assets/inicial.png';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from 'components/Button';
+import { useCallback, useEffect } from 'react';
+import instance from 'common/config/api';
+import { adicionarCategorias } from 'store/reducers/categorias';
+import { adicionarItens } from 'store/reducers/itens';
 
 export default function Home() {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const categorias = useSelector(state => state.categorias);
+
+  const buscarCategorias = useCallback(async () => {
+    const resposta = await instance.get('/categorias')
+
+    dispatch(adicionarCategorias(resposta.data));
+  }, [dispatch]);
+
+  const buscarItens = useCallback(async () => {   
+    const resposta = await instance.get('/itens')
+
+    dispatch(adicionarItens(resposta.data));
+  }, [dispatch]);
+
+
+  useEffect(() => {
+    buscarCategorias();
+    buscarItens();
+  }, [buscarCategorias, buscarItens])
   return (
     <div>
       <Header
