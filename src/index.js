3b36c1bfa1f80107d4bed7eb0679dab0b144d6ff
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-09-16 11:24:24 -0300

    dd

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import Router from 'routes';
 import './index.css';
 import store from './store';
 import { Provider } from 'react-redux';
+import { createStandaloneToast } from '@chakra-ui/toast';
+
+const { ToastContainer, toast } = createStandaloneToast();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <Router />
+    <ToastContainer />
+  </Provider>
 );
--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import categoriasService from 'services/categorias';
 
@@ -7,11 +8,26 @@ export const buscarCategoria = createAsyncThunk(
   'categproas/buscar',
   categoriasService.buscar
 
+=======
+import { createStandaloneToast } from '@chakra-ui/toast';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import categoriasService from 'services/categorias';
+import { resetarCarrinho } from './carrinho';
+
+const { toast } = createStandaloneToast();
+
+const initialState = [];
+
+export const buscarCategorias = createAsyncThunk(
+  'categorias/buscar',
+  categoriasService.buscar
+>>>>>>> Stashed changes
 );
 
 const categoriasSlice = createSlice({
   name: 'categorias',
   initialState,
+<<<<<<< Updated upstream
   reducers: {
     adicionarCategorias: (state, { payload } ) => {
       state.push(...payload)
@@ -26,8 +42,60 @@ const categoriasSlice = createSlice({
       }
     )
    }
+=======
+  extraReducers: builder => {
+    builder
+    .addCase(
+      buscarCategorias.fulfilled,
+      (state, { payload }) => {
+        toast({
+          title: 'Sucesso!',
+          description: 'Categorias carregadas com sucesso!',
+          status: 'success',
+          duration: 2000,
+          isClosable: true
+        })
+        return payload;
+      }
+    )
+    .addCase(
+      buscarCategorias.pending,
+      (state, { payload }) => {
+        toast({
+          title: 'Carregando',
+          description: 'Carregando categorias',
+          status: 'loading',
+          duration: 2000,
+          isClosable: true
+        })
+      }
+    )
+    .addCase(
+      buscarCategorias.rejected,
+      (state, { payload }) => {
+        toast({
+          title: 'Erro',
+          description: 'Erro na busca de categorias',
+          status: 'error',
+          duration: 2000,
+          isClosable: true
+        })
+      }
+    )
+    .addCase(
+      resetarCarrinho.type,
+      () => {
+        toast({
+          title: 'Sucesso!',
+          description: 'Compra completada com sucesso!',
+          status: 'success',
+          duration: 2000,
+          isClosable: true
+        })
+      }
+    )
+  }
+>>>>>>> Stashed changes
 });
-
-export const { adicionarCategorias } = categoriasSlice.actions;
 
 export default categoriasSlice.reducer;
