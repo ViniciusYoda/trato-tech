e6957e09667bea06e931ad0cfb717fc3116c737a
Vinícius Yoda <vyoda4604@gmail.com>
2023-09-22 09:46:25 -0300

    finish

--- src/services/itens.js
+++ src/services/itens.js
@@ -1,11 +1,16 @@
-import instance from "common/config/api";
+import instance from 'common/config/api';
 
 const itensService = {
-    buscar: async () => {
-        const resposta = await instance.get('/itens');
+  buscar: async () => {
+    const resposta = await instance.get('/itens');
 
-        return resposta.data
-    }
+    return resposta.data;
+  },
+  buscarDeCategorias: async (nomeCategoria) => {
+    const resposta = await instance.get(`/itens?categoria=${nomeCategoria}`);
+
+    return resposta.data;
+  }
 }
 
 export default itensService;
