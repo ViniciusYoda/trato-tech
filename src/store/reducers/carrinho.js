2e7ed11c31fdacd601e41c8766d13f213db6bc19
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-09-29 15:34:48 -0300

    chamada

--- src/store/reducers/carrinho.js
+++ src/store/reducers/carrinho.js
@@ -1,6 +1,6 @@
 import { createAction, createSlice } from '@reduxjs/toolkit';
 
-const initialState = [];
+const initialState = { data: [], total: 0 };
 
 export const carregarPagamento = createAction('carrinho/carregarPagamento');
 
@@ -9,26 +9,35 @@ const carrinhoSlice = createSlice({
   initialState,
   reducers: {
     mudarCarrinho: (state, { payload }) => {
-      const temItem = state.some(item => item.id === payload);
-      if (!temItem) return [
-        ...state,
-        {
-          id: payload,
-          quantidade: 1
-        }
-      ];
-      return state.filter(item => item.id !== payload);
+      const temItem = state.data.some(item => item.id === payload);
+      if (!temItem) return {
+        total: state.total,
+        data: [
+          ...state.data,
+          {
+            id: payload,
+            quantidade: 1
+          }
+        ]
+      }
+      return {
+        total: state.total,
+        data: state.data.filter(item => item.id !== payload)
+      };
     },
     mudarQuantidade: (state, { payload }) => {
-      state = state.map(itemNoCarrinho => {
+      state.data = state.data.map(itemNoCarrinho => {
         if(itemNoCarrinho.id === payload.id) itemNoCarrinho.quantidade += payload.quantidade;
         return itemNoCarrinho;
       })
     },
     resetarCarrinho: () => initialState,
+    mudarTotal: (state, { payload }) => {
+      state.total = payload;
+    }
   }
 });
 
-export const { mudarCarrinho, mudarQuantidade, resetarCarrinho } = carrinhoSlice.actions;
+export const { mudarCarrinho, mudarQuantidade, resetarCarrinho, mudarTotal } = carrinhoSlice.actions;
 
 export default carrinhoSlice.reducer;
--- src/store/sagas/carrinho.js
+++ src/store/sagas/carrinho.js
@@ -1,10 +1,38 @@
-import { takeLatest } from 'redux-saga/effects';
+import { call, delay, put, select, takeEvery, takeLatest } from 'redux-saga/effects';
+import bandeirasService from 'services/bandeiras';
+import cartoesService from 'services/cartoes';
+import usuariosService from 'services/usuarios';
 import { carregarPagamento } from 'store/reducers/carrinho';
+import { adicionarUsuario } from '../reducers/usuario';
+import { mudarCarrinho, mudarQuantidade, mudarTotal } from '../reducers/carrinho';
+
+const usuarioLogado = 1;
 
 function* carregarPagamentoSaga() {
-  yield console.log('carregando pagamento');
+  try {
+    const usuario = yield call(usuariosService.buscarPorId, usuarioLogado);
+    const cartoes = yield call(cartoesService.buscarPorIdUsuario, usuarioLogado);
+    const bandeiraIds = cartoes.map(cartao => cartao.bandeiraId);
+    const bandeiras = yield call(bandeirasService.buscarPorId, bandeiraIds);
+    const cartoesComBandeiras = cartoes.map(cartao => {
+      const bandeiraDoCartao = bandeiras.find(bandeira => bandeira.id === cartao.bandeiraId);
+      return { ...cartao, taxa: bandeiraDoCartao.taxa, bandeira: bandeiraDoCartao.nome };
+    });
+    yield put(adicionarUsuario({ ...usuario, cartoes: cartoesComBandeiras }));
+  } catch(e) {}
+}
+
+function* calcularTotal() {
+  yield delay(500);
+  const state = yield select();
+  const total = state.carrinho.data.reduce((total, itemNoCarrinho) => {
+    const item = state.itens.find(item => item.id === itemNoCarrinho.id);
+    return total + item.preco * itemNoCarrinho.quantidade;
+  }, 0);
+  yield put(mudarTotal(total));
 }
 
 export function* carrinhoSaga() {
   yield takeLatest(carregarPagamento, carregarPagamentoSaga);
+  yield takeEvery([mudarQuantidade, mudarCarrinho], calcularTotal);
 }
